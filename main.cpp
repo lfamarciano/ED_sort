7ff14a8c6710f14766150a19063091f59b4ce0ef
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2024-05-20 01:36:01 +0000

    Atualizando a main

--- main.cpp
+++ main.cpp
@@ -1,65 +1,74 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include "dubblelinkedlist.h"
 #include "bubble_sort.h"
 #include "selection_sort.h"
+#include "insertion_sort.h"
 
 using namespace std;
 using namespace std::chrono;
 
+namespace duracaoAlgoritmo
+{
+    enum Algoritmo {BubbleSort, BubbleSortOtimizado, SelectionSort, SelectionSortOtimizado, InsertionSort};
+
+    float tempoExecucao(Node* head, Algoritmo algoritmo)
+    {
+        auto timeStart = high_resolution_clock::now();
+        
+        switch (algoritmo)
+        {
+            case BubbleSort:
+                bubbleSort(head);
+                break;
+            case BubbleSortOtimizado:
+                bubbleSortOtimizado(head);
+                break;
+            case SelectionSort:
+                selectionSort(head);
+                break;
+            case SelectionSortOtimizado:
+                selectionSortOtimizado(head);
+                break;
+            case InsertionSort:
+                insertionSort(head);
+            default:
+                break;
+        }
+
+        auto timeStop = high_resolution_clock::now();
+
+        auto timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
+        return timeDuration.count();
+    }
+}
+
+using namespace duracaoAlgoritmo;
+
 int main()
 {
-    auto timeStart = high_resolution_clock::now();
-    auto timeStop = high_resolution_clock::now();
-    auto timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
-
-    int arriTempoBubble[20];
-    int arriTempoBubbleOptimized[20];
-    int arriTempoSelection[20];
-    int arriTempoSelectionOptimized[20];
-
-
     int randomValue;
     Node* head = nullptr;
+    cout << "BubbleSort; BubbleSortOtimizado; SelectionSort; SelectionSortOtimizado; InserionSort" << endl;
 
-    for (int iNumListas = 0; iNumListas<20; iNumListas++) // testando em 10 listas
+    for (int iNumListas = 0; iNumListas<100; iNumListas++) // testando em 100 listas
     {
         head = nullptr;
         for (int iTamLista = 0; iTamLista < 10000; iTamLista++) // adicionando 10000 valores na lista
         {
-            randomValue = rand() % 501;
+            randomValue = rand() % 20000;
             insertEnd(&head, randomValue);
         }
-        // tempo da bubble sort
-        timeStart = high_resolution_clock::now();
-        bubbleSort(&head);
-        timeStop = high_resolution_clock::now();
-        timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
-        arriTempoBubble[iNumListas] = timeDuration.count();
-
-        // tempo da bubble sort otimizada
-        timeStart = high_resolution_clock::now();
-        optimizedBubbleSort(&head);
-        timeStop = high_resolution_clock::now();
-        timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
-        arriTempoBubbleOptimized[iNumListas] = timeDuration.count();
-
-        // tempo da selection sort
-        timeStart = high_resolution_clock::now();
-        selectionSort(&head);
-        timeStop = high_resolution_clock::now();
-        timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
-        arriTempoSelection[iNumListas] = timeDuration.count();
-
-        // tempo da selection sort otimizada
-        timeStart = high_resolution_clock::now();
-        optimizedSelectionSort(&head);
-        timeStop = high_resolution_clock::now();
-        timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
-        arriTempoSelectionOptimized[iNumListas] = timeDuration.count();
+        cout << tempoExecucao(head, BubbleSort) << ";";
+        cout << tempoExecucao(head, BubbleSortOtimizado) << ";";
+        cout << tempoExecucao(head, SelectionSort) << ";";
+        cout << tempoExecucao(head, SelectionSortOtimizado) << ";";
+        cout << tempoExecucao(head, InserionSort) << endl;
 
         deleteList(&head);
-
+        
+    
     }
     
     return 0;
