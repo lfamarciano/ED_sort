e6f39e8513e279b6cdd5362f8643f514e039d0c4
Luís Felipe Marciano <lfamarciano@gmail.com>
2024-06-09 19:08:48 -0300

    código rodando

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <chrono>
 #include "doubleLinkedList/dubblelinkedlist.h"
 #include "trees/binarytree.h"
+#include "sortingAlgorithms/bubble_sort.h"
+#include "sortingAlgorithms/selection_sort.h"
+#include "sortingAlgorithms/insertion_sort.h"
+#include "sortingAlgorithms/bucket_sort.h"
 
 using namespace std;
 using namespace std::chrono;
@@ -97,16 +101,28 @@ int main()
     int randomValue;
     NodeTree<int>* root1 = nullptr;
     NodeTree<int>* root2 = nullptr;
-    Node<int>* head = nullptr;
+    Node<int>* head1 = nullptr;
+    Node<int>* head2 = nullptr;
+    Node<int>* head3 = nullptr;
+    Node<int>* head4 = nullptr;
+    Node<int>* head5 = nullptr;
+    Node<int>* head6 = nullptr;
+    Node<int>* head7 = nullptr;
     auto timeStart = high_resolution_clock::now();
     auto timeStop = high_resolution_clock::now();
     auto timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
 
-    cout << "Tree Creation Time; List Creation Time; DFS Time; BFS Time; List Search Time" << endl;
+    cout << "Tree Creation Time; List Creation Time; BubbleSort; BubbleSortOtimizado; SelectionSort; SelectionSortOtimizado; InsertionSort; BucketSort; DFS Time; BFS Time; List Search Time" << endl;
 
     for (int iNumListas = 0; iNumListas<100; iNumListas++) // testando em 100 árvores
     {
-        head = nullptr;
+        head1 = nullptr;
+        head2 = nullptr;
+        head3 = nullptr;
+        head4 = nullptr;
+        head5 = nullptr;
+        head6 = nullptr;
+        head7 = nullptr;
         root1 = nullptr;
         root2 = nullptr;
 
@@ -123,18 +139,31 @@ int main()
         timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
         cout << timeDuration.count()/2 << ";";
 
-        timeStart = high_resolution_clock::now();
-        for (int iTamLista = 0; iTamLista < 10000; iTamLista++) // adicionando 10000 valores entre 0 e 100000 em uma lista
+        for (int iTamLista = 0; iTamLista < 10000; iTamLista++) // adicionando 10000 entre 0 e 100 valores na lista
         {
-            randomValue = rand() % 100001;
+            randomValue = rand() % 101;
 
-            insertFront(&head, randomValue);
+            //cria 6 listas iguais
+            insertFront(&head1, randomValue);
+            insertFront(&head2, randomValue);
+            insertFront(&head3, randomValue);
+            insertFront(&head4, randomValue);
+            insertFront(&head5, randomValue);
+            insertFront(&head6, randomValue);
+        }
 
+        timeStart = high_resolution_clock::now();
+        for (int iTamLista = 0; iTamLista < 10000; iTamLista++) // adicionando 10000 entre 0 e 100 valores na lista
+        {
+            randomValue = rand() % 10001;
+
+            //cria 6 listas iguais
+            insertFront(&head7, randomValue);
         }
         timeStop = high_resolution_clock::now();
         timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
         cout << timeDuration.count() << ";";
-        
+
         int sumDFS = 0;
         int sumBFS = 0;
         int sumList = 0;
@@ -142,18 +171,40 @@ int main()
             randomValue = rand() % 100001;
             sumDFS = sumDFS + tempoExecucao(root1, randomValue, DFS);
             sumBFS = sumBFS + tempoExecucao(root2, randomValue, BFS);
-            sumList = sumList + tempoExecucao(head, randomValue, ListSearch);
+            sumList = sumList + tempoExecucao(head7, randomValue, ListSearch);
         }
+
+        cout << tempoExecucao(head1, BubbleSort) << ";";
+        cout << tempoExecucao(head2, BubbleSortOtimizado) << ";";
+        cout << tempoExecucao(head3, SelectionSort) << ";";
+        cout << tempoExecucao(head4, SelectionSortOtimizado) << ";";
+        cout << tempoExecucao(head5, InsertionSort) << ";";
+        cout << tempoExecucao(head6, BucketSort) << ";";
         cout << sumDFS/5 << ";";
         cout << sumBFS/5 << ";";
-        cout << sumList/5 << ";" << endl;
+        cout << sumList/5 << endl;
 
-        deleteList(&head);
+        deleteList(&head1);
+        deleteList(&head2);
+        deleteList(&head3);
+        deleteList(&head4);
+        deleteList(&head5);
+        deleteList(&head6);
         deleteTree(&root1);
         deleteTree(&root2);
     }
 
+    Node<int>* head = nullptr;
 
+    for (int iTamLista = 0; iTamLista < 10000; iTamLista++) // adicionando 10000 entre 0 e 100 valores na lista
+    {
+        int randomValue = rand() % 101;
 
+        //cria 6 listas iguais
+        insertFront(&head, randomValue);
+    }
+
+    // cout << searchNodebyValue(head,1)->payload << endl;
+    // cout << tempoExecucao(head, 1, ListSearch) << endl;
     return 0;
 }
